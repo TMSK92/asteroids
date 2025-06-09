05cdc31e5fa6dd189e2943e0bf8304f74ec48628
TMSK92 <toni.kangasoja@gmail.com>
2025-06-09 18:12:47 +0300

    Updated main and crated circleshape.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +22,9 @@ def main():
 
         screen.fill((0,0,0))
         pygame.display.flip()
+        
+        time = clock.tick(60)
+        dt = time / 1000
 
 if __name__ == "__main__":
     main()
